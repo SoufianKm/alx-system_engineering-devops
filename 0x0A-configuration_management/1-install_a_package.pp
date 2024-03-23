6eb2cfb9bfbc165aa6f7ac15be0e6a7959ef5ffb
SoufianKm <soufian.kamma@gmail.com>
2024-03-22 17:09:13 -0700

    Task 1 - install a package

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,12 @@
 #Install flask from pip3
-package { 'Flask':
-  ensure   => '2.1.0',
-  provider => 'pip3'
+
+package { 'flask':
+  ensure   => 'latest',
+  provider => 'pip3',
 }
+
+package { 'werkzeug':
+  ensure   => 'latest',
+  provider => 'pip3',
+}
+
