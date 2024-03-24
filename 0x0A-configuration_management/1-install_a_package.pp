9a6daf823cf14638e32d9a90944c58a7ba2dce37
SoufianKm <soufian.kamma@gmail.com>
2024-03-24 10:31:52 -0700

    Update installing flask package

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,8 +1,5 @@
-#Install flask from pip3
-
-exec { 'install_flask_and_werkzeug':
-  command     => '/usr/bin/pip3 install flask werkzeug',
-  path        => ['/usr/bin'],
-  environment => ['PIP_REQUIRE_VIRTUALENV=false'],
-  unless      => '/usr/bin/pip3 show flask &>/dev/null && /usr/bin/pip3 show werkzeug &>/dev/null',
+# Install flask from pip3
+package { 'flask':
+  ensure   => '2.1.0',
+  provider => 'pip3'
 }
