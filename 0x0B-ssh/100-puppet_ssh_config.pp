fc4c46ca9a76c973084e3c7336bdfb1eb0013f5d
SoufianKm <soufian.kamma@gmail.com>
2024-03-24 10:41:22 -0700

    Client configuration file (w/ Puppet)

---
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -0,0 +1,16 @@
+# Client configuration file (w/ Puppet)
+# Puppet manifest that changes the config file for ssh
+
+file_line { 'IdentityFile':
+  ensure => 'present',
+  path   => '/etc/ssh/ssh_config',
+  line   => 'IdentityFile ~/.ssh/school',
+  match  => '^IdentityFile'
+}
+
+file_line { 'PasswordAuthentication':
+  ensure => 'present',
+  path   => '/etc/ssh/ssh_config',
+  line   => 'PasswordAuthentication no',
+  match  => '^PasswordAuthentication'
+}
