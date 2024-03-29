584fe18f26e93cbe6c46d394ec21391f161b27d6
SoufianKm <soufian.kamma@gmail.com>
2024-03-29 15:59:47 -0700

    Add a custom HTTP header with Puppet

---
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -0,0 +1,35 @@
+# Puppet Manifest to automatically configure
+# an Nginx server using Puppet instead of Bash
+
+exec { 'apt-get update':
+  command => '/usr/bin/apt-get -y update',
+}
+
+package { 'nginx':
+  ensure  => 'installed',
+  require => Exec['apt-get update'],
+}
+
+file { 'index-html':
+  ensure  => 'present',
+  path    => '/var/www/html/index.html',
+  content => 'Hello World!',
+}
+
+file_line { 'redirect':
+  path    => '/etc/nginx/sites-available/default',
+  after   => 'server_name _;',
+  line    => 'rewrite ^/redirect_me https://www.google.com permanent;',
+  notify  => Service['nginx'],
+  require => Package['nginx'],
+}
+
+exec {'HTTP header':
+	command => 'sed -i "25i\	add_header X-Served-By \$hostname;" /etc/nginx/sites-available/default',
+	provider => 'shell'
+}
+
+service { 'nginx':
+  ensure => running,
+  enable => true,
+}
