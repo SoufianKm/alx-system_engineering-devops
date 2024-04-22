ede7df9337befd7e99679ba0c839204a45c5747e
SoufianKm <soufian.kamma@gmail.com>
2024-04-22 04:09:26 -0700

    Export data to csv format

---
+++ 0x15-api/1-export_to_CSV.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+""" script to export data in the CSV format """
+import requests
+import sys
+import csv
+
+
+if __name__ == "__main__":
+    url = "https://jsonplaceholder.typicode.com/"
+    user = requests.get(url + "users/{}".format(sys.argv[1])).json()
+    todos = requests.get(url + "todos", params={"userId": sys.argv[1]}).json()
+
+    with open("{}.csv".format(sys.argv[1]), "w", newline="") as csvfile:
+        writer = csv.writer(csvfile, quoting=csv.QUOTE_ALL)
+        [writer.writerow([
+                sys.argv[1],
+                user.get("name"),
+                todo.get("completed"),
+                todo.get("title")]
+                )for todo in todos]
