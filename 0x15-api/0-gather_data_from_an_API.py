11dc3b0532412ba35092966aa2e3253af6263eaa
SoufianKm <soufian.kamma@gmail.com>
2024-04-22 03:58:09 -0700

    Gather data from an API

---
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+Script returns information about TODO list progress for a given employee ID
+"""
+
+import requests
+import sys
+import csv
+
+
+if __name__ == "__main__":
+    url = "https://jsonplaceholder.typicode.com/"
+    user = requests.get(url + "users/{}".format(sys.argv[1])).json()
+    todos = requests.get(url + "todos", params={"userId": sys.argv[1]}).json()
+
+    tasks_completed = [todo.get("title") for todo in todos if todo.get(
+        "completed") is True]
+    print("Employee {} is done with tasks({}/{}):".format(
+        user.get("name"), len(tasks_completed), len(todos)))
+    [print("\t {}".format(c)) for completed in tasks_completed]
